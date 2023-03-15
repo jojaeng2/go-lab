25da9332c9534bdb8fe296641779bfda16033055
jojaeng2 <ds4ouj@naver.com>
2023-03-15 21:40:48 +0900

    feat: go variable 선언 실습

---
+++ src/variable/var_keyword.go
@@ -0,0 +1,8 @@
+package main
+
+// func main() {
+// 	var name = "John Doe"           // no type declaration
+// 	var username string = "John123" // type declaration
+// 	var numberOfPosts int           // no initialization
+// 	fmt.Println(numberOfPosts)      // prints 0
+// }
