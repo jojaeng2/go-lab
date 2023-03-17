eab0c1f89a7c22906e8fd38f369104d97832f5e6
jojaeng2 <ds4ouj@naver.com>
2023-03-17 19:51:33 +0900

    feat: docs/method_receivers 학습

--- src/method/method_receiver.go
+++ src/method/method_receiver.go
@@ -1,18 +1,18 @@
 package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-type Bird struct {
-	name string
-}
+// type Bird struct {
+// 	name string
+// }
 
-func (b Bird) Fly() {
-	fmt.Println(b.name, "is flying...")
-}
+// func (b Bird) Fly() {
+// 	fmt.Println(b.name, "is flying...")
+// }
 
-func main() {
-	b := Bird{"Raven"}
-	b.Fly()
-}
+// func main() {
+// 	b := Bird{"Raven"}
+// 	b.Fly()
+// }
