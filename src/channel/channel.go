6b57ae1941d15d4540db99f3b218624cef50b9bb
jojaeng2 <ds4ouj@naver.com>
2023-03-18 16:01:25 +0900

    feat: docs/channel 학습

---
+++ src/channel/channel.go
@@ -0,0 +1,19 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// func SendDataToChannel(ch chan int, value int) {
+// 	ch <- value
+// }
+
+// func main() {
+// 	var v int
+// 	ch := make(chan int) // create channel
+
+// 	go SendDataToChannel(ch, 101)
+
+// 	v = <-ch
+// 	fmt.Println(v)
+// }
---
+++ src/channel/direction-channel.go
@@ -0,0 +1,14 @@
+package main
+
+// func f(ch chan<- int, v int) {
+// 	ch <- v
+// }
+
+// func main() {
+// 	// send-only channel
+// 	ch := make(chan<- int)
+
+// 	go f(ch, 42)
+// 	go f(ch, 41)
+// 	go f(ch, 40)
+// }
